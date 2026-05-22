906d4e4c7e911df300e0d37cf4b0c4053acdb1c1
Lucie Aymeric <luciea3011@gmail.com>
2026-05-22 12:26:10 +0200

    Watch list ready for production

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,12 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root to: "lists#index"
+
+  resources :lists, only: [:index, :show, :new, :create, :destroy] do
+    resources :bookmarks, only: [:new, :create]
+    resources :reviews, only: [:create]
+  end
+
+  resources :bookmarks, only: [:destroy]
 end
